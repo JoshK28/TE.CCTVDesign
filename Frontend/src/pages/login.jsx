80c45b15060e0b82eaf9e7b3e2a0b84233dd4ddd
pierroliketoeat <youngpierro1@gmail.com>
2026-03-16 18:49:34 +1000

    Testing adding to styling for login page. Github commit

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api";
-
+import "../page_styling/login.css"
 
 /*
 The login component provides an interface for users to login and access the project program. It sends the login details to the backend through an API call.
