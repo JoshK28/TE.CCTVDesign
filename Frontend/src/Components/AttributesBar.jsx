2a80bfb37fa874550215f08300768adb318a1ea3
JoshK28 <joshkeenan28@gmail.com>
2026-03-21 22:42:36 +1000

    Creating a new attribute menubar for editing equipment objects

---
+++ Frontend/src/Components/AttributesBar.jsx
@@ -0,0 +1,30 @@
+import { Sidebar } from 'primereact/sidebar';
+
+export default function AttributesBar({ selectedItemId, equipment, onClose }) {
+
+    const selectedItem = equipment.find(item => item.id === selectedItemId);
+
+    return (
+        <div>
+             <Sidebar 
+                visible={itemSelected !== null} 
+                position="right" 
+                onHide={() => setSelectedItem(null)}
+                style={{ width: '300px' }}
+            >
+
+        <h2>Properties</h2>
+        
+        {itemSelected ? (
+          <div className="property-form">
+            <p><strong>Editing Item ID:</strong> {selectedItem.id}</p>
+            <p>More settings coming soon...</p>
+          </div>
+        ) : (
+          <p>Select an item to view its properties.</p>
+        )}
+      </Sidebar>
+    </div>
+       
+    )
+}
