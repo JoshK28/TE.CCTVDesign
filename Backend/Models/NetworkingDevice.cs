94247f567c32732ac3faa8c034570ca39c208c8a
Salad1208 <n11822228@qut.edu.au>
2026-05-05 22:41:33 +1000

    Create tables for access control and networking devices

---
+++ Backend/Models/NetworkingDevice.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class NetworkingDevice
+    {
+        [Key]
+        public int NetworkingID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Manufacturer { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
