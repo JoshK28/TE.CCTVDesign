9f1f334ee1755761652a1110f9d26f640e04a4f6
Salad1208 <n11822228@qut.edu.au>
2026-05-20 19:21:41 +1000

    Migration for adding obstacles and custom cameras

--- Backend/Migrations/20260520034636_AddCustomCamerasAndObstacles.Designer.cs
+++ Backend/Migrations/20260520034636_AddCustomCamerasAndObstacles.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260520034636_AddCustomCamerasAndObstacles")]
+    partial class AddCustomCamerasAndObstacles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -626,6 +629,9 @@ namespace Backend.Migrations
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("datetime2");
 
+                    b.Property<int?>("CreatedById")
+                        .HasColumnType("int");
+
                     b.Property<int>("CreatedByUserId")
                         .HasColumnType("int");
 
@@ -686,9 +692,9 @@ namespace Backend.Migrations
 
                     b.HasKey("CustomCameraId");
 
-                    b.HasIndex("CreatedByUserId");
+                    b.HasIndex("CreatedById");
 
-                    b.ToTable("CustomCameras");
+                    b.ToTable("CustomCamera");
                 });
 
             modelBuilder.Entity("Backend.Models.FloorLayout", b =>
@@ -768,47 +774,6 @@ namespace Backend.Migrations
                     b.HasKey("NetworkingID");
 
                     b.ToTable("NetworkingDevices");
-                });
-
-            modelBuilder.Entity("Backend.Models.Obstacle", b =>
-                {
-                    b.Property<int>("ObstacleId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ObstacleId"));
-
-                    b.Property<string>("Color")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("FloorID")
-                        .HasColumnType("int");
-
-                    b.Property<double>("Height")
-                        .HasColumnType("float");
-
-                    b.Property<string>("Label")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<double>("Rotation")
-                        .HasColumnType("float");
-
-                    b.Property<double>("Width")
-                        .HasColumnType("float");
-
-                    b.Property<double>("X")
-                        .HasColumnType("float");
-
-                    b.Property<double>("Y")
-                        .HasColumnType("float");
-
-                    b.HasKey("ObstacleId");
-
-                    b.HasIndex("FloorID");
-
-                    b.ToTable("Obstacles");
                 });
 
             modelBuilder.Entity("Backend.Models.Project", b =>
@@ -922,8 +887,7 @@ namespace Backend.Migrations
 
                     b.HasOne("Backend.Models.CustomCamera", "CustomCamera")
                         .WithMany()
-                        .HasForeignKey("CustomCameraId")
-                        .OnDelete(DeleteBehavior.SetNull);
+                        .HasForeignKey("CustomCameraId");
 
                     b.HasOne("Backend.Models.FloorLayout", "FloorLayout")
                         .WithMany()
@@ -949,9 +913,7 @@ namespace Backend.Migrations
                 {
                     b.HasOne("Backend.Models.User", "CreatedBy")
                         .WithMany()
-                        .HasForeignKey("CreatedByUserId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
+                        .HasForeignKey("CreatedById");
 
                     b.Navigation("CreatedBy");
                 });
@@ -965,17 +927,6 @@ namespace Backend.Migrations
                         .IsRequired();
 
                     b.Navigation("Project");
-                });
-
-            modelBuilder.Entity("Backend.Models.Obstacle", b =>
-                {
-                    b.HasOne("Backend.Models.FloorLayout", "FloorLayout")
-                        .WithMany()
-                        .HasForeignKey("FloorID")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("FloorLayout");
                 });
 
             modelBuilder.Entity("Backend.Models.Project", b =>
